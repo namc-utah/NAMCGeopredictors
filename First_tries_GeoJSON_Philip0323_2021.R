b3c93e11339d3e2ba74fb298ecc2ef0f028a13cf
alherca73 <55201378+alherca73@users.noreply.github.com>
2021-04-08 10:47:07 -0600

    Testing extraction of Wsheds from DB

--- First_tries_GeoJSON_Philip0323_2021.R
+++ First_tries_GeoJSON_Philip0323_2021.R
@@ -1,5 +1,5 @@
-install.packages("remotes")
-remotes::install_github("namc-utah/NAMCr")
+#install.packages("remotes")
+#remotes::install_github("namc-utah/NAMCr")
 
 library(NAMCr)
 library(sf)
@@ -11,17 +11,12 @@ library(geojsonsf)
 
 ### list of watersheds
 
-purrunga<-read.csv("/Users/alexhernandez/Desktop/GitHubs/NAMCGeopa/sites_with_catchments.csv",header = FALSE)
-purrunga<-c(28305,27902,27992,7135,
-            7162,
-            7140,
-            7146,
-            7149,
-            7154,
-            27911)
+AIMCatch<-read.csv("/Users/alexhernandez/Desktop/GitHubs/NAMCGeopa/2020_AIM_sites_with_catchments.csv",header = TRUE)
+AIMCatch<-as.data.frame(AIMCatch[,1])
+
 pirin<-list()
-for (i in 1:nrow(purrunga)){
-  ii<-purrunga[i,]
+for (i in 1:nrow(AIMCatch)){
+  ii<-AIMCatch[i,]
   print(ii)
   ttcpurrunga<-NAMCr::query("siteInfo", siteId=ii)
   ttcmaje<-ttcpurrunga[["catchment"]] # Extract the geojson object from the list
@@ -34,6 +29,8 @@ for (i in 1:nrow(purrunga)){
 
 
 sftotal<-do.call(rbind, pirin) # get the list components and turn them into a full sf object
+
+mapview(sftotal)
 
 
 for (i in 1:nrow(purrunga)){
