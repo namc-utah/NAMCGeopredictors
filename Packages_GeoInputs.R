ad7ee930a8789173fc98fc67a9faacea26c1de7e
courtwrightj01 <jennifer.courtwright@usu.edu>
2021-11-02 14:16:23 -0500

    fixing file paths for all input files

--- Packages_GeoInputs.R
+++ Packages_GeoInputs.R
@@ -2,27 +2,31 @@
 ## Load useful packages
 library(sf)
 library(raster)
-#library(data.table)
-#setwd("Z://GIS/GIS_Stats")
-setwd("/Users/alexhernandez/Desktop/BUG_BLM/ZonalTest/GIS_Stats01")
-library(here)
-#library(ggpubr)
+library(data.table)
+wd="Z://GIS/GIS_Stats"
+library(ggpubr)
 library(mapview)
-#library(prism)
+library(prism)
 library(exactextractr)
-#library(mapedit)
-#library(reticulate)
+library(mapedit)
+library(reticulate)
 library(rgee)
-#library(tidyverse)
-#library(survival)
+library(tidyverse)
+library(survival)
 library(dplyr)
-#library(nhdplusTools)
-#library(lubridate)
+library(nhdplusTools)
+library(lubridate)
 library(units)
 library(geojsonio)
 library(rmapshaper)
+library(whitebox)
 
 ee_Initialize()
+# if error run
+#ee_install()
+#ee_clean_pyenv()
+# restart R
+#ee_install()
 
 ###### Define predictors GEE
 USGS_NED<-ee$Image("USGS/NED")$select("elevation") # elevation
@@ -57,48 +61,48 @@ slopegee.perc<- slopegee$divide(180)$multiply(3.14159)$tan()$multiply(1)$rename(
 
 ###### Define predictors stored in-house
 ## Climate !!!
-TMIN_WS.ras<-raster(here("Climate/Data/tmin_oldtntp/w001001.adf")) 
-TMIN_UT_WS.ras<-raster(here("Climate/Data/tmin_usgs/w001001.adf"))# UTDEQ version
-#PT_Tmin.ras<-raster(here("/Climate/Data/tmin_usgs","w001001.adf"))
-#TMIN_AVE.ras<-raster(here("/Climate/Data/tmin_usgs","w001001.adf"))# UTDEQ version
-KFACT.ras<-raster(here("Soils/Data/kfact_usgs/w001001.adf"))
-PMIN_WS.ras<-raster(here("/Climate/Data/pmin_usgs","w001001.adf"))
-RH_WS.ras<-raster(here("/Climate/Data/rhmean_usgs","w001001.adf"))# UTDEQ version is RH_AVE
-TMAX_WS.ras<-raster(here("/Climate/Data/tmax_usgs","w001001.adf")) # UTDEQ version is TMAX_AVE
-TMEAN_WS.ras<-raster(here("/Climate/Data/tmean_usgs","w001001.adf"))
-TMEAN_UT_WS.ras<-raster(here("/Climate/Data/tmean_usgsut","w001001.adf"))# UTDEQ version
-XWD_WS.ras<-raster(here("Climate/Data/xwd_usgs/w001001.adf"))
-MEANP_WS.ras<-raster(here("/Climate/Data/meanp_usgs","w001001.adf"))# UTDEQ version
-MAXP_WS.ras<-raster(here("/Climate/Data/pmax_usgs","w001001.adf"))# UTDEQ version
-MAXWD_WS.ras<-raster(here("/Climate/Data/Wdmax_usgs","w001001.adf"))# UTDEQ version
-FST32F_WS.ras<-raster(here("/Climate/Data/fstfrz_usgs","w001001.adf"))# UTDEQ version
-MEANP_PIBO_WS.ras<-raster(here("Climate/Data/meanppt_pibo.tif"))# UTDEQ version)
+TMIN_WS.ras<-raster(paste0(wd,"/Climate/Data/tmin_oldtntp/w001001.adf")) 
+TMIN_UT_WS.ras<-raster(paste0(wd,"/Climate/Data/tmin_usgs/w001001.adf"))# UTDEQ version
+PT_Tmin.ras<-raster(paste0(wd,"/Climate/Data/tmin_usgs/w001001.adf"))
+TMIN_AVE.ras<-raster(paste0(wd,"/Climate/Data/tmin_usgs/w001001.adf"))# UTDEQ version
+KFACT.ras<-raster(paste0(wd,"/Soils/Data/kfact_usgs/w001001.adf"))
+PMIN_WS.ras<-raster(paste0(wd,"/Climate/Data/pmin_usgs/w001001.adf"))
+RH_WS.ras<-raster(paste0(wd,"/Climate/Data/rhmean_usgs/w001001.adf"))# UTDEQ version is RH_AVE
+TMAX_WS.ras<-raster(paste0(wd,"/Climate/Data/tmax_usgs/w001001.adf")) # UTDEQ version is TMAX_AVE
+TMEAN_WS.ras<-raster(paste0(wd,"/Climate/Data/tmean_usgs/w001001.adf"))
+TMEAN_UT_WS.ras<-raster(paste0(wd,"/Climate/Data/tmean_usgsut/w001001.adf"))# UTDEQ version
+XWD_WS.ras<-raster(paste0(wd,"/Climate/Data/xwd_usgs/w001001.adf"))
+MEANP_WS.ras<-raster(paste0(wd,"/Climate/Data/meanp_usgs/w001001.adf"))# UTDEQ version
+MAXP_WS.ras<-raster(paste0(wd,"/Climate/Data/pmax_usgs/w001001.adf"))# UTDEQ version
+MAXWD_WS.ras<-raster(paste0(wd,"/Climate/Data/Wdmax_usgs/w001001.adf"))# UTDEQ version
+FST32F_WS.ras<-raster(paste0(wd,"/Climate/Data/fstfrz_usgs/w001001.adf"))# UTDEQ version
+MEANP_PIBO_WS.ras<-raster(paste0(wd,"/Climate/Data/meanppt_pibo.tif"))# UTDEQ version)
 
 
 ## Atmosphere !!!
-AtmCa.ras<-raster(here("/Atmos/Data/atm_ca","w001001.adf"))
-AtmSO4.ras<-raster(here("/Atmos/Data/atm_so4","w001001.adf"))
-AtmNa.ras<-raster(here("/Atmos/Data/atm_na","w001001.adf"))
-AtmNO3.ras<-raster(here("/Atmos/Data/atm_no3","w001001.adf"))
+AtmCa.ras<-raster(paste0(wd,"/Atmos/Data/atm_ca/w001001.adf"))
+AtmSO4.ras<-raster(paste0(wd,"/Atmos/Data/atm_so4/w001001.adf"))
+AtmNa.ras<-raster(paste0(wd,"/Atmos/Data/atm_na/w001001.adf"))
+AtmNO3.ras<-raster(paste0(wd,"/Atmos/Data/atm_no3/w001001.adf"))
 
 
-Eco3_PT.vec<-st_read(here("Ecoregion/Data/Eco_Level_III_US.shp"))
-Eco4_PT.vec<-st_read(here("Ecoregion/Data/us_eco_l4_no_st.shp"))
-Vol_ave.ras<-raster(here("/Geology/Data/vol","w001001.adf"))
-alru_dom.ras<-raster(here("Vegetation/Data/alru_domrec/w001001.adf"))
-Evergr_ave.ras<-raster(here("Vegetation/Data/evergr/w001001.adf"))
-EVI_AveAve.ras<-raster(here("/Vegetation/Data/evi_ave","w001001.adf"))
-EVI_MAX_AVE.ras<-raster(here("/Vegetation/Data/evi_max_10b.tif"))# UTDEQ version
-CaO_Mean.ras<-raster(here("/Geology/Data/cao_19jan10","w001001.adf"))
-TP_Mean.ras<-raster(here("/Geology/Data/p_19jan10","w001001.adf"))
-AWC_soil.ras<-raster(here("/Soils/Data/awc","w001001.adf"))
-GW_P_Sp_Mx.ras<-raster(here("/Hydro/Data/gw_p_sp","w001001.adf"))
-SOC.ras<-raster(here("/Soils/Data/soc","w001001.adf"))
-Pct_Alfi.ras<-raster(here("/Soils/Data/alfi_nonulls","w001001.adf"))
-Wb_mx_area.vec<-st_read(here("/Metrics/Data","Wb.shp"))
-Kfact.ras<-raster(here("/Soils/Data/kfact_usgs","w001001.adf"))
-Db3rdbar.ras<-raster(here("/Soils/Data/db3rdbar","w001001.adf"))
+Eco3_PT.vec<-st_read(paste0(wd,"/Ecoregions/Data/Eco_Level_III_US.shp"))
+Eco4_PT.vec<-st_read(paste0(wd,"/Ecoregions/Data/us_eco_l4_no_st.shp"))
+Vol_ave.ras<-raster(paste0(wd,"/Geology/Data/vol/w001001.adf"))
+alru_dom.ras<-raster(paste0(wd,"/Vegetation/Data/alru_domrec/w001001.adf"))
+Evergr_ave.ras<-raster(paste0(wd,"/Vegetation/Data/evergr/w001001.adf"))
+EVI_AveAve.ras<-raster(paste0(wd,"/Vegetation/Data/evi_ave/w001001.adf"))
+EVI_MAX_AVE.ras<-raster(paste0(wd,"/Vegetation/Data/evi_max_10b.tif"))# UTDEQ version
+CaO_Mean.ras<-raster(paste0(wd,"/Geology/Data/cao_19jan10/w001001.adf"))
+TP_Mean.ras<-raster(paste0(wd,"/Geology/Data/p_19jan10/w001001.adf"))
+AWC_soil.ras<-raster(paste0(wd,"/Soils/Data/awc/w001001.adf"))
+GW_P_Sp_Mx.ras<-raster(paste0(wd,"/Hydro/Data/gw_p_sp/w001001.adf"))
+SOC.ras<-raster(paste0(wd,"/Soils/Data/soc/w001001.adf"))
+Pct_Alfi.ras<-raster(paste0(wd,"/Soils/Data/alfi_nonulls/w001001.adf"))
+Wb_mx_area.vec<-st_read(paste0(wd,"/Metrics/Data/Wb.shp"))
+Kfact.ras<-raster(paste0(wd,"/Soils/Data/kfact_usgs/w001001.adf"))
+Db3rdbar.ras<-raster(paste0(wd,"/Soils/Data/db3rdbar/w001001.adf"))
 
 ## Additional CO model
-LOG_XP_PT.ras<-raster(here("Metrics/Colorado/data/meanppt/w001001.adf"))
+LOG_XP_PT.ras<-raster(paste0(wd,"/Metrics/Colorado/data/meanppt/w001001.adf"))
 
