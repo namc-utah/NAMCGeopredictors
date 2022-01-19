b73aa769684d34bc0bd29b9e5209ff46563a5322
courtwrightj01 <jennifer.courtwright@usu.edu>
2022-01-19 13:29:11 -0600

    final troublehsooting for rgee dont run ee_install!!

---
+++ R/Rgee install.R
@@ -0,0 +1,20 @@
+# library("reticulate")
+# Sys.setenv(RETICULATE_PYTHON = "C:/Users/jenni/AppData/Local/ESRI/conda/envs/arcgispro-py3-clone-1/python.exe")
+# reticulate::py_config()
+# reticulate::use_python("C:/Users/jenni/AppData/Local/ESRI/conda/envs/arcgispro-py3-clone-1/python.exe")
+# py_discover_config()
+# install(ee)
+# Sys.which("python")
+# np <- reticulate::import("numpy", convert = FALSE)
+# a <- np$array(c(1:4))
+# print(a)
+# ee_install(
+#   py_env = "C:/Users/jenni/AppData/Local/R-MINI~1/envs/r-reticulate",
+#   earthengine_version = ee_version(),
+#   confirm = interactive()
+# )
+#
+# ee_clean_pyenv()
+# py_install("ee")
+# library(rgee)
+# rgee::ee_install()
