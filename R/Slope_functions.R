eff93c87fcf003f1a5b187623345be49562ba488
courtwrightj01 <jennifer.courtwright@usu.edu>
2022-01-28 15:29:15 -0600

    fixing NV slope function to use reticulate

--- R/Slope_functions.R
+++ R/Slope_functions.R
@@ -33,56 +33,77 @@ NHDSLOPE<-function(points2process,geometry_input_path,...){
 }
 
 
-# still in development
+
+#' Watershed slope using flow length- NV MMI model
+#' @description total elevation change in the watershed/ arcgis flow length
+#' @param polygon2process
+#' @param geometry_input_path
+#' @param USGS_NED
+#' @param ...
+#'
+#' @return
+#' @export
+#'
+#' @examples
+Slope_WS<-function(polygon2process,geometry_input_path,USGS_NED,...){
+  #geometry_input_path="Z:/GIS/Delineation/NewPythonMethod/Rasters/NVmod/NVFLD8.tif"
+  # watershed slope = rise/ run
+  ### rise= max watershed elevation - min watershed elevation ###
+  # call elevation functions to get min and max watershed elevations
+  max_watershed_elevation=ELVmax_WS(polygon2process,USGS_NED)
+  min_watershed_elevation=ELVmin_WS(polygon2process,USGS_NED)
+
+  ### run= flow length as determined by ArcGIS from DEM flow length raster ###
+  polygon2processtrans<-sf::st_transform(polygon2process, 5072)# transforming to CRS of NV D8 point Flow Direction
+  #write buffer of watershed out as a shapefile
+  sf::write_sf(polygon2processtrans,"polygon2processtrans.shp")#write it out to your project working directory!
+
+  #import the spatial analyst arc py module/ library to get needed functions
+  sa=reticulate::import("arcpy.sa")
+  # clip the NV flow accumulation grid to the watershed boundary using the spatial analyst extract by mask tool and save to local app data. File path of the new file created will be stored in the extract2 object
+  clippedFlowRaster=sa$ExtractByMask(geometry_input_path,'polygon2processtrans.shp')
+  #calculate flow length using the spatial analyst FlowLength tool and save to local app data. File path of the new file created will be stored in the extract2 object
+  ShedFlowL=sa$FlowLength(clippedFlowRaster,'UPSTREAM')# previous python code had downstream but not sure why!!
+  FlowLength=raster::raster(paste0(ShedFlowL))
+  max_flow_length=raster::maxValue(FlowLength) # code previously multiplied by 10 and then divided by 100. make sure this output number is in proper units (m)
+  media=(max_watershed_elevation-min_watershed_elevation)/max_flow_length
+   return(media)
+}
 
 
-# Slope_WS<-function(polygon2process,...){
-#     validgeobuf<-st_buffer(st_transform(polygon2process, 5072), 300) # transforming to CRS of NV D8 point Flow Direction
-#   write_sf(st_transform(polygon2process, 5072), here("wat.shp"))
-#   write_sf(validgeobuf,here("buffer_wat.shp"))
-#   inputD8<-here("NVMod/NVFLD8.tif")
-#   outputD8<-here("NVMod/NVFLD8_crop3.tif")
-#   reproD8<-gdalUtils::gdalwarp(srcfile =outputD8, dstfile = destD8, t_srs = '+proj=aea +lat_0=23 +lon_0=-96 +lat_1=29.5 +lat_2=45.5 +x_0=0 +y_0=0 +datum=NAD83 +units=m +no_defs')
-#   outputFL<-here("NVMod/NVFL_crop6.tif")
-#   inputwat<-here("wat.shp")
-#   inputpoly2crop<-here("buffer_wat.shp")
-#   destD8<-here("NVMod/NVFLD8_cropCRS.tif")
-#   destD8flow<-here("NVMod/NVFLD8Flow_crop.tif")
-#   destD8stream<-here("NVMod/NVFLD8Stream_crop.tif")
-#   #whitebox::wbt_clip_raster_to_polygon(input = inputD8, polygons = inputpoly2crop, output = outputD8) # clip the DF8 to the watershed bounds
-#   whitebox::wbt_d8_flow_accumulation(outputD8, destD8flow, out_type="cells", pntr = TRUE, esri_pntr = TRUE) #
-#   whitebox::wbt_extract_streams(destD8flow, destD8stream, threshold=5000.0)
-#   whitebox::wbt_downslope_flowpath_length(d8_pntr = outputD8,output = outputFL, watersheds = inputwat )
-#   media<-exact_extract(SOC.ras,polygon2process,'mean')
-#   return(media)
-# }
-#
-# mask<-st_read(inputpoly2crop)
-# ptm <- proc.time()
-# writeRaster(raster::crop(raster::mask(rastrillo, mask),extent(mask)),datatype='INT1U',overwrite=TRUE,filename = here("NVMod/NVFLD8_crop3.tif"))
-# proc.time() - ptm
-#
-# slpavg <- function(polygon2process,USGS_NED,...) {
-# slopegee<-ee$Terrain$slope(USGS_NED) # slope
-# slopegee.perc<- slopegee$divide(180)$multiply(3.14159)$tan()$multiply(1)$rename("percent")#Slope percent
-#   zones.Albers.3 <- zones.Albers.2[, c(1:4)]
-#   #Wsheds.att.OLD$PPT_ACCUM<-NA
-#   zones.Albers.3$SlopeGEE <- NA
-#
-#   for (i in 1:nrow(zones.Albers.3)) {
-#     tryCatch({
-#       #if an error is found then it is printed, but the loop does not break and continues with the next iteration
-#       objecto <- zones.Albers.3[i, ] # Take the first feature
-#       slope.water <-
-#         ee_extract(slopegee.perc,
-#                    objecto,
-#                    fun = ee$Reducer$mean(),
-#                    scale = 30) %>% as_tibble()
-#       print(slope.water)
-#       slope.water <- pull(slope.water)
-#       zones.Albers.3[[6]][i] <- slope.water
-#     }, error = function(e) {
-#       cat("ERROR :", conditionMessage(e), "\n")
-#     })
-#   }
-# }
+
+
+#' Average Watershed slope
+#' @description slope of each cell in a DEM and then averaged across the watershed
+#' @param polygon2process
+#' @param USGS_NED
+#' @param ...
+#'
+#' @return
+#' @export
+#'
+#' @examples
+ slpavg <- function(polygon2process,USGS_NED,...) {
+  slopegee<-ee$Terrain$slope(USGS_NED) # slope
+  slopegee.perc<- slopegee$divide(180)$multiply(3.14159)$tan()$multiply(1)$rename("percent")#Slope percent
+  zones.Albers.3 <- zones.Albers.2[, c(1:4)]
+  #Wsheds.att.OLD$PPT_ACCUM<-NA
+  zones.Albers.3$SlopeGEE <- NA
+
+  for (i in 1:nrow(zones.Albers.3)) {
+    tryCatch({
+      #if an error is found then it is printed, but the loop does not break and continues with the next iteration
+      objecto <- zones.Albers.3[i, ] # Take the first feature
+      slope.water <-
+        ee_extract(slopegee.perc,
+                   objecto,
+                   fun = ee$Reducer$mean(),
+                   scale = 30) %>% as_tibble()
+      print(slope.water)
+      slope.water <- pull(slope.water)
+      zones.Albers.3[[6]][i] <- slope.water
+    }, error = function(e) {
+      cat("ERROR :", conditionMessage(e), "/n")
+    })
+  }
+}
