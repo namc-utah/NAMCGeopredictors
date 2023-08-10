a520f7457e3850f0be6d3f2411143d35ec3fb861
Andrew Caudillo <andrew.caudillo@usu.edu>
2023-08-10 10:04:56 -0600

    adding merging step

--- R/streamstats_API.R
+++ R/streamstats_API.R
@@ -24,3 +24,31 @@ sheds[[i]]<-ws
 
 }
 streamstats::leafletWatershed(sheds[[4]])
+
+setwd('C://Users//andrew.caudillo//Box//NAMC//GIS//Watersheds//streamstats_R')
+
+
+for(i in 1:length(sheds)){
+  streamstats::writeShapefile(watershed = sheds[[i]], layer = paste(x$siteId[i],'shed',sep='_'), dir = "ws_shp", what = "boundary")
+}
+
+#add in routine that combines them into one shapefile here
+#likely will need to read them back in, merge, then write again.
+#because streamstats makes them a "watershed" file, not a shape.
+
+setwd('C://Users//andrew.caudillo//Box//NAMC//GIS//Watersheds//streamstats_R//ws_shp')
+filenames <- list.files(pattern='*.shp')
+filepaths <-filenames
+
+# Read each shapefile and return a list of sf objects
+listOfShp <- lapply(filepaths, st_read)
+
+# Look to make sure they're all in the same CRS
+unique(sapply(listOfShp, st_crs))
+
+# Combine the list of sf objects into a single object
+combinedShp <- do.call(what = sf:::rbind.sf, args=listOfShp)
+
+yy<-do.call(rbind,sheds)
+
+plot(combinedShp$geometry)
