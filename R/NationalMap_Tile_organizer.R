2064424eb493e6dfa993d6bc59833dce38068be6
Andrew Caudillo <andrew.caudillo@usu.edu>
2025-04-17 07:41:00 -0600

    additions for raster plotting

--- R/NationalMap_Tile_organizer.R
+++ R/NationalMap_Tile_organizer.R
@@ -9,3 +9,20 @@ s<-terra::sprc(terra_tiles)
 m<-terra::merge(s)
 
 terra::writeRaster(m,'C://Users//andrew.caudillo.BUGLAB-I9//Box//NAMC WATS Department Files//GIS//DEMs//NationaMap//AZ//AZ_Statewide_DEM.tif')
+
+library(terra)
+AZs<-terra::rast('C://Users//andrew.caudillo.BUGLAB-I9//Box//NAMC WATS Department Files//GIS//DEMs//NationaMap//AZ//AZ_Statewide_DEM.tif')
+
+
+
+terra::plot(Azs)
+#essentially a conditionally formatted raster
+#from the original. (this example is cold vs warm in AZ)
+d<-AZs > 1524
+mappy<-terra::plot(d,legend=F,col=c('firebrick3','dodgerblue4'))
+add_legend('bottomleft',
+           leg=c('Warm','Cold'),
+           pch=22,
+           pt.bg=c('firebrick3','dodgerblue4'),
+           bty='n')
+terra::writeRaster(d,'C://Users//andrew.caudillo.BUGLAB-I9//Box//NAMC//OEModeling//NAMC_Supported_OEmodels//Arizona//AZ_ColdWarm_DEM.tif')
